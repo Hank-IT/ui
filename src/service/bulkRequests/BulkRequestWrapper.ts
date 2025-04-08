7fe78d437e2c9879b72bb6f350579c208fd286af
alexander.hank <mail@hank-it.com>
2025-04-08 17:38:38 +0200

    feat: Added sent state and removed semicolons

--- src/service/bulkRequests/BulkRequestWrapper.ts
+++ src/service/bulkRequests/BulkRequestWrapper.ts
@@ -1,40 +1,46 @@
 import { BaseRequest } from '../requests'
 
-
 // @ts-expect-error
 export class BulkRequestWrapper<T extends BaseRequest> {
-  protected response: any = null;
-  protected error: any = null;
+  protected response: any = null
+  protected error: any = null
+  protected sent: boolean = false
 
   public constructor(protected request: T) {}
 
   public async send(signal?: AbortSignal) {
     try {
-      this.response = await this.request.setAbortSignal(signal).send();
+      this.response = await this.request.setAbortSignal(signal).send()
     } catch (err) {
-      console.error(err);
+      console.error(err)
 
-      this.error = err;
+      this.error = err
     }
+
+    this.sent = true
   }
 
   public isLoading(): boolean {
-    return this.request.isLoading();
+    return this.request.isLoading()
   }
 
   public getResponse() {
-    return this.response;
+    return this.response
   }
 
   public getError() {
-    return this.error;
+    return this.error
   }
 
   public getRequest(): T {
-    return this.request;
+    return this.request
   }
 
   public hasError(): boolean {
-    return Boolean(this.getError());
+    return Boolean(this.getError())
+  }
+
+  public wasSent(): boolean {
+    return this.sent
   }
 }
