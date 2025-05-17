a284ac8bd719b4a6d8e9a403a80629c0f7fccce4
alexander.hank <mail@hank-it.com>
2025-05-17 09:52:57 +0200

    fix: Added BulkRequestExecutionMode enum to export

--- src/service/bulkRequests/index.ts
+++ src/service/bulkRequests/index.ts
@@ -1,5 +1,11 @@
 import { BulkRequestWrapper } from './BulkRequestWrapper'
 import { BulkRequestSender } from './BulkRequestSender'
 import { BulkRequestEventEnum } from './BulkRequestEvent.enum'
+import { BulkRequestExecutionMode } from './BulkRequestSender'
 
-export { BulkRequestEventEnum, BulkRequestWrapper, BulkRequestSender }
+export {
+  BulkRequestEventEnum,
+  BulkRequestWrapper,
+  BulkRequestSender,
+  BulkRequestExecutionMode
+}
