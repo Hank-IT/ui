dbe27a97af666aa9c18ce26180c080eef16470ff
alexander.hank <mail@hank-it.com>
2025-06-14 18:09:44 +0200

    feat: Move persistence drivers to own service and make them more generic

--- src/vue/forms/types/PersistenceDriver.ts
+++
@@ -1,7 +0,0 @@
-import { type PersistedForm } from './PersistedForm'
-
-export interface PersistenceDriver {
-  get<T>(key: string): PersistedForm<T> | null
-  set<T>(key: string, form: PersistedForm<T>): void
-  remove(key: string): void
-}
