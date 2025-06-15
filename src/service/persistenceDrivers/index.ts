39bf3ace7e61e4bb0815f8f54950b34fdbcfb5f0
alexander.hank <mail@hank-it.com>
2025-06-15 14:06:46 +0200

    fix: Fix imports in persistenceDrivers

--- src/service/persistenceDrivers/index.ts
+++ src/service/persistenceDrivers/index.ts
@@ -1,7 +1,7 @@
 import { LocalStorageDriver } from './LocalStorageDriver'
 import { NonPersistentDriver } from './NonPersistentDriver'
 import { SessionStorageDriver } from './SessionStorageDriver'
-import { PersistenceDriver } from './types/PersistenceDriver'
+import { type PersistenceDriver } from './types/PersistenceDriver'
 
 export { LocalStorageDriver, NonPersistentDriver, SessionStorageDriver }
 
